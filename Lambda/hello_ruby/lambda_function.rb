9dac0226c5b41b83fcec31e7c4c6a451912c1350
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-05-06 20:04:18 +0000

    Actualiza lambda_handler para regresar más info

--- Lambda/hello_ruby/lambda_function.rb
+++ Lambda/hello_ruby/lambda_function.rb
@@ -1,6 +1,21 @@
 require 'json'
 
 def lambda_handler(event:, context:)
-    # TODO implement
-    { statusCode: 200, body: JSON.generate('Hello Ruby!') }
+  nombre = event.dig('queryStringParameters', 'nombre') || 'Ruby'
+  {
+    statusCode: 200,
+    body: JSON.generate({
+      'code' => 'OK',
+      'message' => "Hello #{nombre}!",
+      'httpMethod' => event['httpMethod'] || 'No tiene',
+      'path' => event['path'] || 'No tiene',
+      'qryStrParams' => event['queryStringParameters'] || 'No hay!',
+      #'headers' => event['headers'],
+      'memory' => context.memory_limit_in_mb
+    }),
+    headers: {
+      'Content-Type' => 'text/plain',
+      'X-Mi-Mensaje' => 'Yo soy Juan Camaney'
+    }
+  }
 end
